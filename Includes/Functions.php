c822dadcbcfd45fa7fda9672077d852a9bca9f2b
Abderrahmane <abderrahmanahlalaly76@gmail.com>
2025-01-03 22:08:16 +0100

    Ajouter function de connexion et fonction pour vérifier l'email

---
+++ Includes/Functions.php
@@ -0,0 +1,45 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Includes/Connect.php';
+
+$msg = null;
+function is_email_exist($email){
+    global $con, $msg;
+    try{
+        $stmt = $con->prepare("SELECT email FROM users WHERE email = ?");
+        $stmt->bindParam(1, $email, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        
+        if ($result) {
+            $msg = "l'email est déjà existe";
+            echo "<script>alert('$msg')</script>";
+            return true;
+        }
+    }catch(PDOException $e){
+        return false;
+    }
+}
+function register($f_name, $l_name, $email, $pwd){
+    global $con, $msg;
+    $visiteur = $_SERVER['DOCUMENT_ROOT'] . '/views/visiteur.php';
+    if (!is_email_exist($email)) {
+        try{
+            $pwd_hashed = password_hash($pwd, PASSWORD_DEFAULT);
+            $stmt = $con->prepare("INSERT INTO users(f_name, l_name, email, password_hashed) VALUES (?,?,?,?)");
+            $stmt->bindParam(1, $f_name, PDO::PARAM_STR);
+            $stmt->bindParam(2, $l_name, PDO::PARAM_STR);
+            $stmt->bindParam(3, $email, PDO::PARAM_STR);
+            $stmt->bindParam(4, $pwd_hashed, PDO::PARAM_STR);
+            $stmt->execute();
+            header('location:') . $visiteur;
+            $msg = "Inscription Réussie";
+            return "<script>alert('$msg')</script>";
+        }catch(PDOException $e){
+            $msg = $e->getMessage();
+            return "<script>alert('$msg')</script>";
+        }
+    }else{
+        $msg = "Email déjà existe";
+        return "<script>alert('$msg')</script>";
+    }
+}
