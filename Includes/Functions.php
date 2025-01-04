7b540ab7b8cf761711c1030c01d73372f4fa5326
Abderrahmane <abderrahmanahlalaly76@gmail.com>
2025-01-04 13:21:32 +0100

    Ajoute la page Visitor.php avec function logout.php

--- Includes/Functions.php
+++ Includes/Functions.php
@@ -1,5 +1,7 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/Includes/Connect.php';
+
+require_once 'Connect.php';
+
 
 $msg = null;
 function is_email_exist($email){
@@ -23,23 +25,15 @@ function register($f_name, $l_name, $email, $pwd){
     global $con, $msg;
     $visiteur = $_SERVER['DOCUMENT_ROOT'] . '/views/visiteur.php';
     if (!is_email_exist($email)) {
-        try{
-            $pwd_hashed = password_hash($pwd, PASSWORD_DEFAULT);
-            $stmt = $con->prepare("INSERT INTO users(f_name, l_name, email, pwd_hashed) VALUES (?,?,?,?)");
-            $stmt->bindParam(1, $f_name, PDO::PARAM_STR);
-            $stmt->bindParam(2, $l_name, PDO::PARAM_STR);
-            $stmt->bindParam(3, $email, PDO::PARAM_STR);
-            $stmt->bindParam(4, $pwd_hashed, PDO::PARAM_STR);
-            $stmt->execute();
-            header('location:') . $visiteur;
-            $msg = "Inscription Réussie";
-            return "<script>alert('$msg')</script>";
-        }catch(PDOException $e){
-            $msg = $e->getMessage();
-            return "<script>alert('$msg')</script>";
-        }
-    }else{
-        $msg = "Email déjà existe";
+        $pwd_hashed = password_hash($pwd, PASSWORD_DEFAULT);
+        $stmt = $con->prepare("INSERT INTO users(f_name, l_name, email, password_hashed) VALUES (?,?,?,?)");
+        $stmt->bindParam(1, $f_name, PDO::PARAM_STR);
+        $stmt->bindParam(2, $l_name, PDO::PARAM_STR);
+        $stmt->bindParam(3, $email, PDO::PARAM_STR);
+        $stmt->bindParam(4, $pwd_hashed, PDO::PARAM_STR);
+        $stmt->execute();
+        header('location:') . $visiteur;
+        $msg = "Inscription Réussie";
         return "<script>alert('$msg')</script>";
     }
 }
@@ -59,30 +53,22 @@ function get_role($email){
 function login($email, $pass){
     global $con;
     if (is_email_exist($email)) {
-        $stmt = $con->prepare("SELECT pwd_hashed, roles FROM users WHERE email = :email");
+        $stmt = $con->prepare("SELECT password_hashed, roles FROM users WHERE email = :email");
         $stmt->bindParam(':email', $email);
         $stmt->execute();
 
         $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($pass && password_verify($pass, $resultat['pwd_hashed'])) {
+        if ($pass && password_verify($pass, $resultat['password_hashed'])) {
             session_start();
             $_SESSION['email'] = $email;
             $_SESSION['roles'] = $resultat['roles'];
-            switch(strtolower($resultat['roles'])){
-                case 'admin':
-                    header('location: admin.php');
-                    break;
-                case 'author':
-                    header('location: author.php');
-                    break;
-                case 'visiteur':
-                    header('location: visiteur.php');
-                    break;
-                default:
-                    header('location: login.php');
-                    break;
-            }
+            header('location : /Views/Visitor.php');
         }
     }
     return false;
+}
+function logout(){
+    session_unset();
+    session_destroy();
+    header('location: login.php');
 }
